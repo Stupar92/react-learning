9bcae5d35e268a98fe80bffe1806213536bb022f
Jovan Stupar (E-Search) <a-jostup@microsoft.com>
2016-11-14 20:22:25 +0000

    Session 2 done

--- modern-react-with-redux/Sections-1/src/index.js
+++ modern-react-with-redux/Sections-1/src/index.js
@@ -1,16 +1,41 @@
 // Create new component. This component should produce some HTML
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchBar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/videoList'
+import VideoDetail from './components/videoDetail'
 
 const API_KEY = "AIzaSyDx2f98jiKeF1te6HJsi6ttAbxxh_kF-mw";
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
+        
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
+            this.setState({
+                videos: data,
+                selectedVideo: data[0]
+            });
+        });
+   }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail  video={this.state.selectedVideo}/>
+                <VideoList
+                    videos={this.state.videos}
+                    onVideoSelect={(selectedVideo) => this.setState({selectedVideo: selectedVideo})} />
+            </div>
+        );
+    }
 }
 
 //Comonent needs to be inserted into the DOM
