0706ab2a040c3636db2d1a4b2fac31c30ab90c45
Jovan Stupar (E-Search) <a-jostup@microsoft.com>
2016-11-15 15:13:27 +0000

    Redux begining

--- modern-react-with-redux/Sections-4/src/reducers/index.js
+++ modern-react-with-redux/Sections-4/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import BookReduces from './reducerBooks';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BookReduces
 });
 
 export default rootReducer;
---
+++ modern-react-with-redux/Sections-4/src/reducers/reducerBooks.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        { title: 'Book Example 1'},
+        { title: 'Book Example 2'},
+        { title: 'Book Example 3'},
+        { title: 'Book Example 4'},
+        { title: 'Book Example 5'}
+    ];
+}
