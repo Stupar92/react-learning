958e3d4a0183a7eb383aa69e7db3e32e168273d6
Stupar92 <stupar.etf@gmail.com>
2016-11-28 21:27:51 +0100

    Session 9 done 11-28-2016

--- modern-react-with-redux/Sections-7/src/components/postsNew.js
+++ modern-react-with-redux/Sections-7/src/components/postsNew.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
+    
+    // Go up the parents tree and find router property (our react router from index.js)
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    onSubmit(props) {
+        /*These are not props from our component but props from the form*/
+        this.props.createPost(props)
+            .then(() => {
+                //blog post has been created, navigate the user to index
+                this.context.router.push('/');
+            });
+    }
+
     render() {
-        //const handleSubmit = this.props.handleSubmit;
+        //we have method handleSubmit from redux form.
+        //same as: const handleSubmit = this.props.handleSubmit;
         const { fields: { title, categories, content }, handleSubmit} = this.props;
         return (
-            <form onSubmit = { handleSubmit(this.props.createPost) }>
+            <form onSubmit = { handleSubmit(this.onSubmit.bind(this)) }>
                 <h3>Create a New Post</h3>
-                <div className="form-group">
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
                     <div className="text-help">
@@ -17,17 +34,24 @@ class PostsNew extends Component {
                     </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories}/>
+                     <div className="text-help">
+                        {categories.touched ? categories.error : ""}
+                    </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <textarea className="form-control" {...content}/>
+                     <div className="text-help">
+                        {content.touched ? content.error : ""}
+                    </div>
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -38,6 +62,12 @@ function validate(values) {
 
     if (!values.title) {
         errors.title = 'Enter a title';
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter categories';
+    }
+    if (!values.content) {
+        errors.content = 'Enter some content';
     }
 
     return errors;
