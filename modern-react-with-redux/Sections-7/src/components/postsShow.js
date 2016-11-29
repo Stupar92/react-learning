38216dd4dfab4705ea3824e09f6d33816a43110d
Jovan Stupar <v-jovstu@microsoft.com>
2016-11-29 08:01:31 +0000

    Session 10 done 11-29-2016

--- modern-react-with-redux/Sections-7/src/components/postsShow.js
+++ modern-react-with-redux/Sections-7/src/components/postsShow.js
@@ -1,17 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchPost } from '../actions/index';
-
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
      componentWillMount() {
         this.props.fetchPost(this.props.params.id);
     }
 
+    onDeleteClick() {
+        this.props.deletePost(this.props.params.id)
+            .then(() => {
+                 this.context.router.push('/');
+            });
+    }
+
     render() {
-        return <div>Show post {this.props.params.id}</div>
+        if (!this.props.post) {
+            return <div>Loading...</div>
+        }
+        const post = this.props.post;
+
+        //we can get id param from url by: console.log(this.props.params.id);
+        return (
+            <div>
+                <Link to="/">Back to index</Link>
+                <button className="btn btn-danger"
+                    onClick={this.onDeleteClick.bind(this)}>
+                    Delete Post
+                </button>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
+        );
     }
 }
 
-export default connect(null, {fetchPost})(PostsShow);
+function mapStateToProps(state) {
+    return { post: state.posts.post};
+}
+
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
