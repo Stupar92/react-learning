1b6e39d3ba7f4ff0415e8cf2713ad5697ca136a1
Jovan Stupar <v-jovstu@microsoft.com>
2016-12-13 15:38:12 +0100

    Session 14 done 12-13-2016 Section2 completed

---
+++ advanced-react-with-redux/Section-2-testing/src/reducers/comments.js
@@ -0,0 +1,15 @@
+import { SAVE_COMMENT } from '../actions/types';
+
+export default function(state = [], action) {
+    
+    if (!action) {
+        return state;
+    }
+    
+    switch(action.type) {
+        case SAVE_COMMENT:
+            return [ ...state, action.payload];
+    }
+
+    return state;
+}
