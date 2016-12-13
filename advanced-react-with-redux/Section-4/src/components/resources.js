c1a48d1d93af65eb47acd5b4f0e764bad2a0dfab
Jovan Stupar <v-jovstu@microsoft.com>
2016-12-13 20:40:33 +0100

    Session 16 done 12-13-2016

---
+++ advanced-react-with-redux/Section-4/src/components/resources.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+        Protected Resource
+        <ul>
+            <li>S</li>
+            <li>T</li>
+            <li>U</li>
+            <li>P</li>
+            <li>A</li>
+            <li>R</li>
+        </ul>
+        </div>
+    )
+}
