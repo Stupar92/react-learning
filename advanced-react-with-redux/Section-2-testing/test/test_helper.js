5039d0e1d47dc82dc71968f0293fdc4618cdfda7
Jovan Stupar <v-jovstu@microsoft.com>
2016-12-13 18:05:18 +0100

    Session 15 done 12-13-2016 Section3 completed

--- advanced-react-with-redux/Section-2-testing/test/test_helper.js
+++ advanced-react-with-redux/Section-2-testing/test/test_helper.js
@@ -1,36 +1,44 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import jsdom from 'jsdom'; //emulate DOM inside a terminal
+import _$ from 'jquery'; // if we used $ it would try to access the DOM right away
 import TestUtils from 'react-addons-test-utils';
-import jsdom from 'jsdom';
+import ReactDOM from 'react-dom';
 import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+    // 1. Set up testing environment to run like a browser in command line
+    // we assign jsdom's emulated DOM to global.document. global in node is the same as window in the browser, i.e. global scope,
+    // so when jquery boots up it will get fake references to document, thinking it runs inside a browser
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
 global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
 
-chaiJquery(chai, chai.util, $);
+const $ = _$(global.window); // jqury, just be responsible for this piece of DOM that we are giving you
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
+// 2. Build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass, props={}, state={}) {
   const componentInstance =  TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
-      <ComponentClass {...props} />
-    </Provider>
-  );
+       <Provider store={createStore(reducers, state)}>
+         <ComponentClass {...props} />
+       </Provider>
+     );
 
+  //ReactDOM.findDOMNode(componentInstance); -> this produces html
   return $(ReactDOM.findDOMNode(componentInstance));
-}
+};
 
+// 3. Build helper for simulating events
 $.fn.simulate = function(eventName, value) {
   if (value) {
     this.val(value);
   }
   TestUtils.Simulate[eventName](this[0]);
 };
+
+// 4. Set up chai-jquery
+chaiJquery(chai, chai.util, $);
+
 
 export {renderComponent, expect};
