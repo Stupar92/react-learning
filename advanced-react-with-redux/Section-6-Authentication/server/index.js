71f7769d71befbf068ffa6ee84b241e895d133e1
Jovan Stupar <v-jovstu@microsoft.com>
2016-12-23 17:22:41 +0100

    Session 19 done 12-23-2016

--- advanced-react-with-redux/Section-6-Authentication/server/index.js
+++ advanced-react-with-redux/Section-6-Authentication/server/index.js
@@ -3,18 +3,21 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router');
-
+const mongoose = require('mongoose');
 const app = express();
 
-// App setup
+/*** DB Setup  ***/
+mongoose.connect('mongodb://localhost:auth/auth');
 
+/*** App setup ***/
 //both morgan and body parsers are middlewares, with 'app.use' we are registering them on the app
 app.use(morgan('combined')); //logging middleware
 app.use(bodyParser.json({ type: '*/*'})); // used to parse incoming requests into json 
 router(app);
 
-// Server setup
+/*** Server setup ***/
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
+
 console.log('Server listening on: ', port);
