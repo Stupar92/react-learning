85f080b36cb2d7272c3a1218d39925f3ce720791
Jovan Stupar <v-jovstu@microsoft.com>
2016-12-24 15:49:17 +0100

    Session 20 done 12-24-2016

--- advanced-react-with-redux/Section-6-Authentication/server/controllers/authentication.js
+++ advanced-react-with-redux/Section-6-Authentication/server/controllers/authentication.js
@@ -5,7 +5,30 @@ exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
+    if (!email || !password) {
+        return res.status(422).send({error: 'You must provide email or password'});
+    }
+
     User.findOne({email: email}, function(err, existingUser) {
-        
+        if (err) {
+            return next(err);
+        }
+
+        if (existingUser) {
+            return res.status(422 /*unprocessable entity*/).send({error: 'Email is in use'});
+        }
+
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        user.save(function(err) {
+            if (err) {
+                return next(err);
+            }
+
+            return res.json({success: true});
+        });
     });
 }
